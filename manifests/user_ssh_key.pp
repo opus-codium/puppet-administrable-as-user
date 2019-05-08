28ef51bb99e74b196b6195c5ff4f844a1a637a6c
Romuald Conty <romuald@opus-codium.fr>
2019-05-08 19:51:24 +0200

    Implement 'user_ssh_key' defined type

--- manifests/user_ssh_key.pp
+++ manifests/user_ssh_key.pp
@@ -1,9 +1,21 @@
-# @summary A short summary of the purpose of this defined type.
+# @summary Ease user's SSH key declaration
 #
-# A description of what this defined type does
+# This defined type declares authorized SSH key through built-in ssh_authorized_key{} but uses the string format used by SSH public key file.
+#
+# In simplier words, just copy and paste your ~/.ssh/id_*.pub content to this type.
 #
 # @example
-#   administrable_as_user::user_ssh_key { 'namevar': }
+#   administrable_as_user::user_ssh_key { 'ssh-rsa YOUR_KEY user@host':
+#     user => 'dummy',
+#   }
 define administrable_as_user::user_ssh_key (
+  String $user,
 ) {
+  $ssh_key = $name.split(' ')
+  ssh_authorized_key { $ssh_key[2]:
+    ensure => present,
+    user   => $user,
+    type   => $ssh_key[0],
+    key    => $ssh_key[1],
+  }
 }
