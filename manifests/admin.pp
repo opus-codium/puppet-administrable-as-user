39c226381b3f1df77bed539b800d11b701e2cb54
Romuald Conty <romuald@opus-codium.fr>
2019-05-08 19:40:44 +0200

    Implement 'admin' defined type

--- manifests/admin.pp
+++ manifests/admin.pp
@@ -1,9 +1,35 @@
-# @summary A short summary of the purpose of this defined type.
+# @summary Setup an admin account
 #
-# A description of what this defined type does
+# This defined type is used to declare each administrator.
+#
+# It creates user account (set shell, create home directory, etc.) and add declared SSH keys.
 #
 # @example
-#   administrable_as_user::admin { 'namevar': }
+#   administrable_as_user::admin { 'adminisraptor':
+#     shell => '/bin/zsh',
+#   }
 define administrable_as_user::admin (
+  Enum['present', 'absent'] $ensure = present,
+  String $password = '*',
+  String $shell = '/bin/bash',
+  Array[String] $ssh_keys = [],
 ) {
+  user { $name:
+    ensure         => $ensure,
+    groups         => [
+      'adm',
+      'sudo',
+    ],
+    shell          => $shell,
+    home           => "/home/${name}",
+    managehome     => true,
+    password       => $password,
+    purge_ssh_keys => true,
+  }
+
+  $ssh_keys.each |$user_ssh_key| {
+    administrable_as_user::user_ssh_key { $user_ssh_key:
+      user => $name,
+    }
+  }
 }
